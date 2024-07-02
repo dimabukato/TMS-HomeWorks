f968caec107e94485e2be8bf9dc9121ab99815bc
dimabukato <dimas.bukato1@yandex.by>
2024-07-02 18:52:12 +0300

    Добавлен калькулятор

---
+++ Calculat/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Calculat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First number: ");
+            string firstNumber = Console.ReadLine();
+            Console.WriteLine("Second number: ");
+            string secondNumber = Console.ReadLine();
+            if (double.TryParse(firstNumber, out double firstNum)) 
+            {
+                if (double.TryParse(secondNumber, out double secondNum))
+                {
+                    Console.WriteLine("Choose: '+' '-' '*' '/' '%'");
+                    switch (Console.ReadLine())
+                    {
+                        case "+":
+                            
+                            Console.WriteLine($"Your result is:{firstNum + secondNum}");
+                            break;
+
+                        case "-":
+                            Console.WriteLine($"Your result is:{firstNum - secondNum}");
+                            break;
+
+                        case "*":
+                            Console.WriteLine($"Your result is:{firstNum * secondNum}");
+                            break;
+
+                        case "/":
+                            Console.WriteLine($"Your result is:{firstNum / secondNum}");
+                            break;
+
+                        case "%":
+                            Console.WriteLine($"Your result is:{firstNum % secondNum}");
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown operaion, please try again");
+                            break;
+                    }
+                }
+                else
+                Console.WriteLine("Input data must be numbers");
+            }
+            else 
+            Console.WriteLine("Input data must be numbers");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ConsoleApp1
+﻿namespace Calculat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I");
         }
     }
 }
