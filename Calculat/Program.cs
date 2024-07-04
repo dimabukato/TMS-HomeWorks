2e5282961483d60421671b67b04c2671af32bfdf
dimabukato <dimas.bukato1@yandex.by>
2024-07-04 18:08:32 +0300

    Final calculator ?

--- Calculat/Program.cs
+++ Calculat/Program.cs
@@ -4,48 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("First number: ");
-            string firstNumber = Console.ReadLine();
-            Console.Write("Second number: ");
-            string secondNumber = Console.ReadLine();
-            if (double.TryParse(firstNumber, out double firstNum)) 
+            
+            while (true)
             {
-                if (double.TryParse(secondNumber, out double secondNum))
+                Console.Write("First number: ");
+                string firstNumber = Console.ReadLine();
+                Console.Write("Second number: ");
+                string secondNumber = Console.ReadLine();
+                if (double.TryParse(firstNumber, out double firstNum) && double.TryParse(secondNumber, out double secondNum))
                 {
-                    Console.WriteLine("Choose: '+' '-' '*' '/' '%'");
-                    switch (Console.ReadLine())
-                    {
-                        case "+":
-                            
-                            Console.WriteLine($"Your result:{firstNum + secondNum}");
-                            break;
+                        Console.WriteLine("Choose: '+' '-' '*' '/' '%'");
+                        switch (Console.ReadLine())
+                        {
+                            case "+":
 
-                        case "-":
-                            Console.WriteLine($"Your result:{firstNum - secondNum}");
-                            break;
+                                Console.WriteLine($"Your result:{firstNum + secondNum}");
+                                break;
 
-                        case "*":
-                            Console.WriteLine($"Your result:{firstNum * secondNum}");
-                            break;
+                            case "-":
+                                Console.WriteLine($"Your result:{firstNum - secondNum}");
+                                break;
 
-                        case "/":
-                            Console.WriteLine($"Your result:{firstNum / secondNum}");
-                            break;
+                            case "*":
+                                Console.WriteLine($"Your result:{firstNum * secondNum}");
+                                break;
 
-                        case "%":
-                            Console.WriteLine($"Your result:{firstNum % secondNum}");
-                            break;
+                            case "/":
+                                Console.WriteLine($"Your result:{firstNum / secondNum}");
+                                break;
 
-                        default:
-                            Console.WriteLine("Unknown operaion");
-                            break;
-                    }
+                            case "%":
+                                Console.WriteLine($"Your result:{firstNum % secondNum}");
+                                break;
+
+                            default:
+                                Console.WriteLine("Unknown operaion");
+                                break;
+                        }
                 }
                 else
-                Console.WriteLine("Unknown operaion");
+                {
+                    Console.WriteLine("Unknown operaion");
+                }
+                Console.WriteLine("Сontinue calculations?");
+                switch (Console.ReadLine())
+                {  
+                    case "yes":
+                        break;
+
+                   case "no":
+                        return;
+                }
+
             }
-            else 
-            Console.WriteLine("Unknown operaion");
         }
     }
 }
